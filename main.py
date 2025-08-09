90eee893111b1d35e3f3f3693939bc1edba9dfa4
GuestUser <guest@git_test.co.jp>
2025-08-09 11:37:57 +0900

    sum関数を使用

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from word_util import check_word
 if __name__=="__main__":
     print(check_word("acde"))
-    sum=0
-    for num in range(1,11):
-        sum=sum+num
+    sum=sum(range(1,11))
     print(sum)    
