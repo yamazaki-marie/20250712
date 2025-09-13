ee5da866e5c1f302ed32778a78a7728aa73522f4
GuestUser <guest@git_test.co.jp>
2025-09-13 11:48:13 +0900

    20250913

---
+++ test_class.py
@@ -0,0 +1,11 @@
+class Human:
+    def __init__(self,name):
+        self.name=name
+        self.seibetu=1
+        self.length=160
+    def show_my_info():
+        info="私の名前は"+self.name+"です"
+        return info
+yamazaki=Human("i")
+info=yamazaki.show_my_info(self) 
+print(info)
